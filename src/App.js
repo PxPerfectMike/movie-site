bb4fc43802ac88a6df93035c4357c13f388a30fc
Michael Baker <pxperfectmike@gmail.com>
2022-06-08 21:14:45 -0700

    add unique ids

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useId } from "react";
 import './App.css';
 import searchIcon from './search.svg';
 import MovieCard from "./MovieCard";
@@ -9,6 +9,8 @@ const API_URL = 'https://www.omdbapi.com?apikey=9d041017';
 const App = () => {
     const [movies, setMovies] = useState([]);
     const [searchTerm, setSearch] = useState('');
+
+    const id = useId();
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
@@ -45,7 +47,7 @@ const App = () => {
                     ? (
                         <div className='container'>
                             {movies.map((movie) =>
-                                <MovieCard movie={movie} />
+                                <MovieCard key={id} movie={movie} />
                             )}
                         </div>
                     ) : (
